4fcccaa96c103fe3c9d31511e66616c16412af07
stargateaudio <stargateaudio@noreply.github.com>
2021-07-27 22:29:07 -0700

    Initial work on the Mac port

---
+++ scripts/homebrew_deps.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -x
+
+brew install \
+	ffmpeg \
+	fftw \
+	flac \
+	jq \
+	lame \
+	libogg \
+	libsndfile \
+	libvorbis \
+	opus \
+	portaudio \
+	portmidi \
+	python@3.9 \
+	rubberband
+
