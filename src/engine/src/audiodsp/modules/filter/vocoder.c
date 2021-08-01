1d3bb335b132b414f687fb1cd68e92c4b72679ee
stargateaudio <stargateaudio@noreply.github.com>
2021-08-01 13:33:34 -0700

    Tighter vocoder sound

--- src/engine/src/audiodsp/modules/filter/vocoder.c
+++ src/engine/src/audiodsp/modules/filter/vocoder.c
@@ -9,7 +9,7 @@ void g_vdr_band_init(
     SGFLT a_pitch,
     SGFLT a_res
 ){
-    SGFLT f_release = (1.0f / f_pit_midi_note_to_hz(a_pitch)) * 100.0f;
+    SGFLT f_release = (1.0f / f_pit_midi_note_to_hz(a_pitch));
     g_svf_init(&self->m_filter, a_sr);
     v_svf_set_res(&self->m_filter, a_res);
     v_svf_set_cutoff_base(&self->m_filter, a_pitch);
@@ -31,22 +31,38 @@ void g_vdr_init(t_vdr_vocoder * self, SGFLT a_sr){
     SGFLT f_inc = (f_pit_hz_to_midi_note(7200.0f) - f_freq) /
         (SGFLT)VOCODER_BAND_COUNT;
 
-    for(f_i = 0; f_i < VOCODER_BAND_COUNT; ++f_i)
-    {
-        g_vdr_band_init(&self->bands[f_i], a_sr, f_freq, -0.01f);
+    for(f_i = 0; f_i < VOCODER_BAND_COUNT; ++f_i){
+        g_vdr_band_init(
+            &self->bands[f_i],
+            a_sr,
+            f_freq,
+            -2.1f
+        );
         f_freq += f_inc;
     }
 
-    g_vdr_band_init(&self->low_band, a_sr,
-        f_pit_hz_to_midi_note(200.0f), -15.0f);
-    g_vdr_band_init(&self->high_band, a_sr,
-        f_pit_hz_to_midi_note(6000.0f), -18.0f);
+    g_vdr_band_init(
+        &self->low_band,
+        a_sr,
+        f_pit_hz_to_midi_note(200.0f),
+        -6.0f
+    );
+    g_vdr_band_init(
+        &self->high_band,
+        a_sr,
+        f_pit_hz_to_midi_note(7500.0f),
+        -6.0f
+    );
 
 }
 
-void v_vdr_run(t_vdr_vocoder * self, SGFLT a_mod_in0, SGFLT a_mod_in1,
-        SGFLT a_input0, SGFLT a_input1)
-{
+void v_vdr_run(
+    t_vdr_vocoder * self,
+    SGFLT a_mod_in0,
+    SGFLT a_mod_in1,
+    SGFLT a_input0,
+    SGFLT a_input1
+){
     int f_i;
     SGFLT f_env_val;
     t_state_variable_filter * f_m_filter;
@@ -57,14 +73,15 @@ void v_vdr_run(t_vdr_vocoder * self, SGFLT a_mod_in0, SGFLT a_mod_in1,
     self->output0 = 0.0f;
     self->output1 = 0.0f;
 
-    for(f_i = 0; f_i < VOCODER_BAND_COUNT; ++f_i)
-    {
+    for(f_i = 0; f_i < VOCODER_BAND_COUNT; ++f_i){
         f_m_filter = &self->bands[f_i].m_filter;
         f_envf = &self->bands[f_i].env_follower;
         f_c_filter = &self->bands[f_i].c_filter;
 
-        f_env_val =
-            v_svf_run_2_pole_bp(f_m_filter, f_mono_input);
+        f_env_val = v_svf_run_2_pole_bp(
+            f_m_filter,
+            f_mono_input
+        );
         v_enf_run(f_envf, f_env_val);
 
         v_svf2_run_2_pole_bp(f_c_filter, a_input0, a_input1);
@@ -76,8 +93,7 @@ void v_vdr_run(t_vdr_vocoder * self, SGFLT a_mod_in0, SGFLT a_mod_in1,
     f_envf = &self->low_band.env_follower;
     f_c_filter = &self->low_band.c_filter;
 
-    f_env_val =
-        v_svf_run_2_pole_lp(f_m_filter, f_mono_input);
+    f_env_val = v_svf_run_2_pole_lp(f_m_filter, f_mono_input);
     v_enf_run(f_envf, f_env_val);
 
     v_svf2_run_2_pole_lp(f_c_filter, a_input0, a_input1);
@@ -92,8 +108,7 @@ void v_vdr_run(t_vdr_vocoder * self, SGFLT a_mod_in0, SGFLT a_mod_in1,
     f_envf = &self->high_band.env_follower;
     f_c_filter = &self->high_band.c_filter;
 
-    f_env_val =
-        v_svf_run_2_pole_hp(f_m_filter, f_mono_input);
+    f_env_val = v_svf_run_2_pole_hp(f_m_filter, f_mono_input);
     v_enf_run(f_envf, f_env_val);
 
     v_svf2_run_2_pole_hp(f_c_filter, a_input0, a_input1);
