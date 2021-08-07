907b12e86c770a8054bf01211918b65028809685
stargateaudio <stargateaudio@noreply.github.com>
2021-08-07 11:44:41 -0700

    New project dialog improvements

--- src/sgui/project.py
+++ src/sgui/project.py
@@ -19,21 +19,27 @@ def new_project(a_parent=None):
     try:
         f_last_dir = DEFAULT_PROJECT_DIR
         while True:
-            f_file = QFileDialog.getExistingDirectory(
+            f_file, _filter = QFileDialog.getSaveFileName(
                 a_parent,
-                _('Create a directory for a new project'),
+                _('Create a new project'),
                 f_last_dir,
-                QFileDialog.ShowDirsOnly | QFileDialog.DontUseNativeDialog,
+                options=(
+                    QFileDialog.Option.ShowDirsOnly
+                    |
+                    QFileDialog.Option.DontUseNativeDialog
+                ),
             )
             if f_file and str(f_file):
                 f_file = str(f_file)
                 f_last_dir = f_file
-                if (
-                    not util.check_for_empty_directory(f_file)
-                    or
-                    not util.check_for_rw_perms(f_file)
-                ):
+                if os.path.exists(f_file):
+                    QMessageBox.warning(
+                        a_parent,
+                        "Error",
+                        f"{f_file} already exists"
+                    )
                     continue
+                os.makedirs(f_file)
                 f_file = os.path.join(
                     f_file,
                     f"{MAJOR_VERSION}.project",
