2eb72bb3b77e34957348393dd308e85bc81887eb
stargateaudio <stargateaudio@noreply.github.com>
2021-08-24 20:42:29 -0700

    Add missing files

---
+++ src/test/sglib/models/test_theme.py
@@ -0,0 +1,18 @@
+from sglib.models import theme
+
+SCALER = theme.UIScaler(
+    2000.,
+    1000.,
+    2000.,
+    1000.,
+    1.5,
+)
+
+def test_ui_scaler_pct_to_px():
+    for args, expected in (
+        ((10.,), 100),
+        ((50., 'w'), 1000),
+    ):
+        result = SCALER.pct_to_px(*args)
+        assert result == expected, (result, expected)
+
