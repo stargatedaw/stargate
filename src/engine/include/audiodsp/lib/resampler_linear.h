ed441bef66a8e1bccaab8c3c0e6604965b48829e
stargateaudio <stargateaudio@noreply.github.com>
2021-09-28 00:49:05 -0700

    Stuff

--- src/engine/include/audiodsp/lib/resampler_linear.h
+++ src/engine/include/audiodsp/lib/resampler_linear.h
@@ -5,29 +5,32 @@
 
 typedef void (*resample_generate)(void*);
 
+// Generates audio from a source function at a different (or same) sample
+// rate as Stargate, and resamples the audio using linear interpolation
 struct ResamplerLinear {
     // 1 if there is no need to resample, otherwise 0
     int same;
     int first;
-    // Difference of internal vs. target, decrements at each tick of
+    // Difference of internal vs. target clocks, decrements at each tick of
     // the internal clock
     double pos;
     double inc;
     SGFLT last;
     SGFLT next;
     resample_generate func;
+    void* func_arg;
 };
 
 void resampler_linear_init(
     struct ResamplerLinear* self,
     int internal_rate,
     int target_rate,
-    resample_generate func
+    resample_generate func,
+    void* func_arg
 );
 
 SGFLT resampler_linear_run_mono(
-    struct ResamplerLinear* self,
-    void* func_arg
+    struct ResamplerLinear* self
 );
 
 #endif
