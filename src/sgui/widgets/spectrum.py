4347c6e914dbafc138819b880be42504eb65caa1
stargateaudio <stargateaudio@noreply.github.com>
2021-10-06 00:47:56 -0700

    CPU optimizations for the spectrum analyzer drawing

--- src/sgui/widgets/spectrum.py
+++ src/sgui/widgets/spectrum.py
@@ -18,17 +18,18 @@ class spectrum(QGraphicsPathItem):
         f_low = _shared.EQ_LOW_PITCH
         f_high = _shared.EQ_HIGH_PITCH
         f_width_per_point = (self.spectrum_width / float(f_high - f_low))
-        f_fft_low = float(util.SAMPLE_RATE) / 4096.0
+        f_fft_low = float(util.SAMPLE_RATE) * 0.00024414 # / 4096.0
         f_nyquist = float(util.NYQUIST_FREQ)
+        nyquist_recip = 1. / f_nyquist
         f_i = f_low
         while f_i < f_high:
             f_hz = pitch_to_hz(f_i) - f_fft_low
-            f_pos = int((f_hz / f_nyquist) * len(self.values))
+            f_pos = int((f_hz * nyquist_recip) * len(self.values))
             f_val = float(self.values[f_pos])
             f_db = lin_to_db(f_val) - 64.0
-            f_db += ((f_i - f_low) / 12.0) * 3.0
+            f_db += ((f_i - f_low) * 0.08333333) * 3.0 # / 12.
             f_db = clip_value(f_db, -70.0, 0.0)
-            f_val = 1.0 - ((f_db + 70.0) / 70.0)
+            f_val = 1.0 - ((f_db + 70.0) * 0.0142857142) # / 70.
             f_x = f_width_per_point * (f_i - f_low)
             f_y = f_val * self.spectrum_height
             self.painter_path.lineTo(f_x, f_y)
