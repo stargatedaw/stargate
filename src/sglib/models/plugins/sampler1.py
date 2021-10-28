2e5efab5570d908ea4e2e091fad463ca549fb13c
stargateaudio <stargateaudio@noreply.github.com>
2021-10-28 07:45:02 -0700

    Sampler1: Fix soft crash
    When garbage collecting audio items on an empty instance

--- src/sglib/models/plugins/sampler1.py
+++ src/sglib/models/plugins/sampler1.py
@@ -13,9 +13,12 @@ def get_audio_pool_uids(a_plugin_uid):
     )
     if os.path.isfile(f_file_path):
         f_file = plugin_file(f_file_path)
-        return set(
-            int(x)
-            for x in f_file.configure_dict['load'].split("|")
-            if x
-        )
+        if 'load' in f_file.configure_dict:
+            return set(
+                int(x)
+                for x in f_file.configure_dict['load'].split("|")
+                if x
+            )
+        else:
+            return set()
 
