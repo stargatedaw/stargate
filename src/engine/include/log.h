710a0d1e8192c102e6dd51460a0795ff05e0a922
stargateaudio <stargateaudio@noreply.github.com>
2021-11-01 01:41:21 -0700

    Compile time checking of logger format string and var. args.

--- src/engine/include/log.h
+++ src/engine/include/log.h
@@ -1,8 +1,8 @@
 #ifndef SG_LOG_H
 #define SG_LOG_H
 
-void log_info(const char* format, ...);
-void log_warn(const char* format, ...);
-void log_error(const char* format, ...);
+void log_info(const char* format, ...) __attribute__((format(printf, 1, 2)));
+void log_warn(const char* format, ...) __attribute__((format(printf, 1, 2)));
+void log_error(const char* format, ...) __attribute__((format(printf, 1, 2)));
 
 #endif
--- src/engine/src/log.c
+++ src/engine/src/log.c
@@ -8,7 +8,10 @@ inline void _log_format(char* buffer, char* prefix, const char* format){
     sprintf(buffer, "%s%s\n", prefix, format);
 }
 
-void log_info(const char * format, ...){
+void log_info(
+    const char * format,
+    ...
+){
     va_list args;
     char buffer[strlen(format) + 20];
     _log_format(buffer, "", format);
@@ -17,7 +20,10 @@ void log_info(const char * format, ...){
     va_end (args);
 }
 
-void log_warn(const char * format, ...){
+void log_warn(
+    const char * format,
+    ...
+){
     va_list args;
     char buffer[strlen(format) + 20];
     _log_format(buffer, "WARNING: ", format);
@@ -26,7 +32,10 @@ void log_warn(const char * format, ...){
     va_end(args);
 }
 
-void log_error(const char * format, ...){
+void log_error(
+    const char * format,
+    ...
+){
     va_list args;
     char buffer[strlen(format) + 20];
     _log_format(buffer, "", format);
