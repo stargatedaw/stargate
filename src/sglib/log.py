8b8da07927ed0a2b29a692ddd734afae0ded10d8
stargateaudio <stargateaudio@noreply.github.com>
2021-11-03 20:35:20 -0700

    Logging: Hide the user's home folder
    ...and otherwise create a framework to improve log privacy and avoid
    inadvertantly collecting personal information.

--- src/sglib/log.py
+++ src/sglib/log.py
@@ -6,7 +6,7 @@ import traceback
 
 from logging.handlers import RotatingFileHandler
 
-from sglib.constants import LOG_DIR
+from sglib.constants import LOG_DIR, USER_HOME
 
 __all__ = [
     'LOG',
@@ -16,6 +16,28 @@ LOG = logging.getLogger(__name__)
 FORMAT = (
     '[%(asctime)s] %(levelname)s %(pathname)-30s: %(lineno)s - %(message)s'
 )
+
+
+class RedactingFilter(logging.Filter):
+    def __init__(self, patterns: dict):
+        super(RedactingFilter, self).__init__()
+        self._patterns = patterns
+
+    def filter(self, record):
+        record.msg = self.redact(record.msg)
+        record.pathname = self.redact(record.pathname)
+        if isinstance(record.args, dict):
+            for k in record.args.keys():
+                record.args[k] = self.redact(record.args[k])
+        else:
+            record.args = tuple(self.redact(arg) for arg in record.args)
+        return True
+
+    def redact(self, msg):
+        msg = isinstance(msg, str) and msg or str(msg)
+        for k, v in self._patterns.items():
+            msg = msg.replace(k, v)
+        return msg
 
 def namer(name):
     return f"{name}.gz"
@@ -40,6 +62,12 @@ def setup_logging(
         stream=stream,
     )
     handler.setFormatter(fmt)
+    log.addFilter(
+        RedactingFilter({
+            USER_HOME.replace('\\', '/'): "~",
+            USER_HOME.replace('/', '\\'): "~",
+        })
+    )
     log.addHandler(handler)
 
     handler = RotatingFileHandler(
