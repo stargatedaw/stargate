e2df50dbd390ee3fbb1407939185f1e0a12d6db8
stargateaudio <stargateaudio@noreply.github.com>
2021-11-11 07:34:10 -0800

    Fix rpm.py

--- scripts/rpm.py
+++ scripts/rpm.py
@@ -168,7 +168,6 @@ xdg-mime default stargate.desktop text/stargate.project || true
 %attr(755, root, root) %{{_usr}}/bin/{0}
 %attr(755, root, root) %{{_usr}}/bin/{0}-engine
 %attr(755, root, root) %{{_usr}}/bin/{0}-engine-dbg
-%attr(755, root, root) %{{_usr}}/bin/{0}-paulstretch
 %attr(755, root, root) %{{_usr}}/bin/{0}-sbsms
 %{{_usr}}/share/
 
