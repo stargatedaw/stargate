e4289fd9231efb2e15cb4c072efd0a657132e535
stargateaudio <stargateaudio@noreply.github.com>
2021-11-13 16:48:51 -0800

    Fix linkage test

--- src/macos/test-without-brew.sh
+++ src/macos/test-without-brew.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-# Temporarily rename Homebrew lib directories to test
-# Simulates a Mac without Homebrew installed
+# Remove some brew dependencies to test that Pyinstaller linkage is
+# working as expected
 
-sudo mv /usr/local/lib /usr/local/lib-bkp
-sudo mv /usr/local/opt /usr/local/opt-bkp
+brew uninstall -y portaudio portmidi
 
 open -b io.github.stargateaudio
 
-sudo mv /usr/local/lib-bkp /usr/local/lib
-sudo mv /usr/local/opt-bkp /usr/local/opt
+brew install -y portaudio portmidi
 
