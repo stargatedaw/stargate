e8a135b9264cc684ef279416304e647d61997008
stargateaudio <stargateaudio@noreply.github.com>
2021-11-13 17:11:08 -0800

    Homebrew dependency updates

--- scripts/homebrew_deps.sh
+++ scripts/homebrew_deps.sh
@@ -1,23 +1,25 @@
 #!/bin/sh -x
 
 # Used for creating the app bundle and dmg
-# Note that "System Integrity Protection" and Homebrew's insistenc
+# Note that "System Integrity Protection" and Homebrew's insistence
 # on not running as root makes it impossible to directly install
 # Stargate DAW from a Himebrew tap
 
 brew install \
-    create-dmg \
+	create-dmg \
 	ffmpeg \
 	fftw \
 	flac \
 	jq \
 	lame \
 	libogg \
+	libsamplerate \
 	libsndfile \
 	libvorbis \
 	numpy \
 	opus \
 	portaudio \
 	portmidi \
-	rubberband
+	rubberband \
+	theora
 
