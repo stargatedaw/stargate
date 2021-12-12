5de4bbd7a158564c3e6722dd084e73502dbe8116
stargateaudio <stargateaudio@noreply.github.com>
2021-12-11 20:51:37 -0800

    Add a script for OpenSUSE deps

---
+++ scripts/opensuse_deps.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -x
+
+# This script installs all dependencies needed to build on Fedora
+
+#dependencies
+sudo zypper install \
+    alsa-devel \
+    fftw3-devel \
+    gcc \
+    gcc-c++ \
+    gcovr \
+    gettext \
+    git \
+    jq \
+    lame \
+    libsndfile-devel \
+    portaudio-devel \
+    portmidi-devel \
+    python3-Cython \
+    python3-devel \
+    python3-numpy \
+    python3-qt5 \
+    python3-pytest \
+    python3-pytest-cov \
+    rubberband-cli \
+    vorbis-tools
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+pip install -r ${SCRIPT_DIR}/../src/requirements.txt
+pip install yq
