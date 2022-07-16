1c117623c743baebcdf5f5866221f6811c616904
stargateaudio <stargateaudio@noreply.github.com>
2022-07-16 16:17:04 -0700

    typo

--- scripts/homebrew_deps.sh
+++ scripts/homebrew_deps.sh
@@ -3,7 +3,7 @@
 # Used for creating the app bundle and dmg
 # Note that "System Integrity Protection" and Homebrew's insistence
 # on not running as root makes it impossible to directly install
-# Stargate DAW from a Himebrew tap
+# Stargate DAW from a Homebrew tap
 
 brew install \
     create-dmg \
