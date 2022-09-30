e8ffe1b6e2494558678c48e0d26f7dc469f91506
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-09-30 23:51:00 +0000

    Adding tarfile member sanitization to extractall()

--- src/sgui/project_recovery.py
+++ src/sgui/project_recovery.py
@@ -82,7 +82,26 @@ class ProjectRecoveryWidget(QListWidget):
         )
         shutil.move(f_project_dir, f_tmp_dir)
         with tarfile.open(f_tar_path, "r:bz2") as f_tar:
-            f_tar.extractall(self.project_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(f_tar, self.project_dir)
         shutil.rmtree(f_tmp_dir)
         QMessageBox.warning(
             self,
