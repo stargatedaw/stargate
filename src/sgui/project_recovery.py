9fa0e26763847c5a4da944731cf316e4afb120f1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 01:18:52 +0000

    Adding numeric_owner as keyword arguement

--- src/sgui/project_recovery.py
+++ src/sgui/project_recovery.py
@@ -98,7 +98,7 @@ class ProjectRecoveryWidget(QListWidget):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(f_tar, self.project_dir)
