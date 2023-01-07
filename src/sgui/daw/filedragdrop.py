ed5cb4bffe856126cbdc01647277275f4012aadf
Anon McHacker <anonymous@noreply.L3337.github.com>
2023-01-07 06:27:04 -0800

    Audio file preview: Improved error handling and messages

--- src/sgui/daw/filedragdrop.py
+++ src/sgui/daw/filedragdrop.py
@@ -1,5 +1,5 @@
 from sglib import constants
-from sgui import widgets
+from sgui import shared as glbl_shared, widgets
 from sgui.daw import shared
 from sglib.lib import util
 from sgui.widgets import FileDragDropListWidget
@@ -22,17 +22,27 @@ class FileDragDropper(widgets.AbstractFileBrowserWidget):
         self.stop_preview_button.pressed.connect(self.on_stop_preview)
 
     def on_preview(self):
-        f_list = self.list_file.selectedItems()
-        if f_list:
+        def exactly_one(msg='You must select exactly one audio file'):
+            if glbl_shared.IS_PLAYING:
+                return
+            QMessageBox.warning(None, None, msg)
+        _list = self.list_file.selectedItems()
+        if len(_list) == 0:
+            exactly_one()
+            return
+        if len(_list) > 1:
+            exactly_one()
+            return
+        fname = _list[0].text()
+        if os.path.splitext(fname)[1].lower() in ('.mid', '.midi'):
+            exactly_one(
+                'You must select exactly one audio file, MIDI files '
+                'cannot be previewed'
+            )
+            return
+        if fname:
             constants.IPC.preview_audio(
-                os.path.join(
-                    *(
-                        str(x) for x in (
-                            self.last_open_dir,
-                            f_list[0].text()
-                        )
-                    )
-                )
+                os.path.join(self.last_open_dir, fname)
             )
 
     def on_stop_preview(self):
