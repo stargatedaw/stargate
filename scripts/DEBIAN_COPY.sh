998fc0b131be407ce80c2053d8fa5fc558edfb49
stargateaudio <stargateaudio@noreply.github.com>
2023-01-20 00:24:06 -0800

    Add Linux packaging script
    That I have been hoarding for a long time

---
+++ scripts/DEBIAN_COPY.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -xe
+
+# Build the rpm and deb packages and copy to /mnt/stargatedaw
+# Meant for building the rpm and deb packages on an old version of Debian,
+# so that they will be linked against an old glibc and in theory run on
+# any Linux distro that uses those package formats
+
+cd $(realpath $(dirname $0)/..)
+
+git checkout main
+git pull
+./scripts/deb.py -i
+./scripts/rpm.py
+
+MINOR=$(jq -r .version.minor src/meta.json)
+cp \
+	src/stargate-${MINOR}-amd64.deb \
+	stargate-${MINOR}-1.x86_64.rpm \
+	/mnt/stargatedaw/
