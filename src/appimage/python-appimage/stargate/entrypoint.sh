ba4d0a00c0a56f68eb2ddebcdbcb55b6eb8106a4
Anon McHacker <anonymous@noreply.L3337.github.com>
2023-04-29 12:52:50 -0700

    AppImage: Fix for Fedora 38
    Absolutely no idea why this is suddenly a problem, but adding
    LD_LIBRARY_PATH hackery to the entrypoint script fixes it.

--- src/appimage/python-appimage/stargate/entrypoint.sh
+++ src/appimage/python-appimage/stargate/entrypoint.sh
@@ -1,3 +1,8 @@
 #! /bin/bash -i
+
+if [ -z "$LD_LIBRARY_PATH" ]; then
+    export LD_LIBRARY_PATH="${APPDIR}/usr/lib:${APPDIR}/usr/lib/x86_64-linux-gnu"
+fi
+
 {{ python-executable }} \
 	-u "${APPDIR}/opt/stargate/scripts/stargate" "$@"
