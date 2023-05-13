e7ab4d8ddd3fb868f087f9db4e2c2df30a76845f
Anon McHacker <anonymous@noreply.L3337.github.com>
2023-05-13 09:05:37 -0700

    Use / for Windows path separator

--- src/sglib/lib/path/windows.py
+++ src/sglib/lib/path/windows.py
@@ -5,14 +5,15 @@ INSTALL_PREFIX = os.path.abspath(
         os.path.dirname(__file__),
         *(['..'] * 3),
     ),
-)
-FONTS_DIR = os.path.join(INSTALL_PREFIX, 'files', 'fonts')
-SHARE_DIR = os.path.join(INSTALL_PREFIX, 'files', 'share')
-PRESETS_DIR = os.path.join(INSTALL_PREFIX, 'files', 'presets')
-PLUGIN_ASSETS_DIR = os.path.join(INSTALL_PREFIX, 'files', 'plugin_assets')
-THEMES_DIR = os.path.join(INSTALL_PREFIX, 'files', 'themes')
-COMMIT_PATH = os.path.join(INSTALL_PREFIX, "COMMIT")
-META_DOT_JSON_PATH = os.path.join(INSTALL_PREFIX, "meta.json")
-BIN_DIR = os.path.join(INSTALL_PREFIX, 'scripts')
-ENGINE_DIR = os.path.join(INSTALL_PREFIX, 'engine')
+).replace('\\', '/')
+
+FONTS_DIR = '/'.join((INSTALL_PREFIX, 'files', 'fonts'))
+SHARE_DIR = '/'.join((INSTALL_PREFIX, 'files', 'share'))
+PRESETS_DIR = '/'.join((INSTALL_PREFIX, 'files', 'presets'))
+PLUGIN_ASSETS_DIR = '/'.join((INSTALL_PREFIX, 'files', 'plugin_assets'))
+THEMES_DIR = '/'.join((INSTALL_PREFIX, 'files', 'themes'))
+COMMIT_PATH = '/'.join((INSTALL_PREFIX, "COMMIT"))
+META_DOT_JSON_PATH = '/'.join((INSTALL_PREFIX, "meta.json"))
+BIN_DIR = '/'.join((INSTALL_PREFIX, 'scripts'))
+ENGINE_DIR = '/'.join((INSTALL_PREFIX, 'engine'))
 
