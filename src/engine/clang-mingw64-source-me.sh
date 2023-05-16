8c4627dc7896cd0b81c3e2480134f37c042c51bf
Anon McHacker <anonymous@noreply.L3337.github.com>
2023-05-15 18:24:39 -0700

    Fix incorrect C++ compiler name

--- src/engine/clang-mingw64-source-me.sh
+++ src/engine/clang-mingw64-source-me.sh
@@ -3,7 +3,7 @@
 export PATH="/mingw64/bin:${PATH}"
 PREFIX=x86_64-w64-mingw32
 export CC=clang
-export CXX=clang-c++
+export CXX=clang++
 export CPP=clang-cpp
 export RANLIB=$PREFIX-ranlib
 export PREFIX=/usr
