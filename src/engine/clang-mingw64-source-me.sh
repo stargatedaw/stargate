20d73c61196df55453d62bfbe3293bd130a63ac5
Anon McHacker <anonymous@noreply.L3337.github.com>
2023-05-19 23:13:01 -0700

    Fix ranlib variable

--- src/engine/clang-mingw64-source-me.sh
+++ src/engine/clang-mingw64-source-me.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 export PATH="/mingw64/bin:${PATH}"
-PREFIX=x86_64-w64-mingw32
 export CC=clang
 export CXX=clang++
 export CPP=clang-cpp
-export RANLIB=$PREFIX-ranlib
+export RANLIB=llvm-ranlib
 export PREFIX=/usr
 
