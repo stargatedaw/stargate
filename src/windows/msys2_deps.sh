c76c01240b7ee872126cf2f01217edd8d7b9fdeb
Anon McHacker <anonymous@noreply.L3337.github.com>
2023-10-28 23:28:59 -0700

    fixes

--- src/windows/msys2_deps.sh
+++ src/windows/msys2_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-pacman -S \
+pacman -Sy \
     make \
     mingw-w64-x86_64-clang \
     mingw-w64-x86_64-ffmpeg \
@@ -18,5 +18,20 @@ pacman -S \
     mingw-w64-x86_64-portmidi \
     mingw-w64-x86_64-python \
     mingw-w64-x86_64-rubberband \
-    parallel
+    mingw-w64-ucrt-x86_64-clang \
+    mingw-w64-ucrt-x86_64-ffmpeg \
+    mingw-w64-ucrt-x86_64-fftw \
+    mingw-w64-ucrt-x86_64-flac \
+    mingw-w64-ucrt-x86_64-gcc \
+    mingw-w64-ucrt-x86_64-jq \
+    mingw-w64-ucrt-x86_64-lame \
+    mingw-w64-ucrt-x86_64-libogg \
+    mingw-w64-ucrt-x86_64-libsndfile \
+    mingw-w64-ucrt-x86_64-libvorbis \
+    mingw-w64-ucrt-x86_64-lldb \
+    mingw-w64-ucrt-x86_64-opus \
+    mingw-w64-ucrt-x86_64-portaudio \
+    mingw-w64-ucrt-x86_64-portmidi \
+    mingw-w64-ucrt-x86_64-python \
+    mingw-w64-ucrt-x86_64-rubberband
 
