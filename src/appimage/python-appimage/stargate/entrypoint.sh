a3abf4d279a9d8fe3bc8e7f3c51ecb1172d09f80
Anon McHacker <anonymous@noreply.L3337.github.com>
2023-10-29 18:53:12 -0700

    AppImage: Default to bundled platform plugins

--- src/appimage/python-appimage/stargate/entrypoint.sh
+++ src/appimage/python-appimage/stargate/entrypoint.sh
@@ -4,5 +4,7 @@ if [ -z "$LD_LIBRARY_PATH" ]; then
     export LD_LIBRARY_PATH="${APPDIR}/usr/lib:${APPDIR}/usr/lib/x86_64-linux-gnu"
 fi
 
+export QT_QPA_PLATFORM_PLUGIN_PATH="${APPDIR}/opt/python3.10/lib/python3.10/site-packages/PyQt5/Qt5/plugins/platforms"
+
 {{ python-executable }} \
 	-u "${APPDIR}/opt/stargate/scripts/stargate" "$@"
