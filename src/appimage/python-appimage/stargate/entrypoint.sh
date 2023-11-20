e07340403eb9a6ab9c4e02e338ab2943e0a9ab66
Anon McHacker <anonymous@noreply.L3337.github.com>
2023-11-19 19:24:48 -0800

    AppImage: Do not load user Python site-packages
    Avoids some kind of Qt mismatch depending on what version of Python is
    installed on the system vs. the AppImage

--- src/appimage/python-appimage/stargate/entrypoint.sh
+++ src/appimage/python-appimage/stargate/entrypoint.sh
@@ -8,5 +8,5 @@ fi
 
 export QT_QPA_PLATFORM_PLUGIN_PATH="${APPDIR}/opt/python${PYVER}/lib/python${PYVER}/site-packages/PyQt5/Qt5/plugins/platforms"
 
-_USE_PYQT5=1 {{ python-executable }} \
+{{ python-executable }} -s \
 	-u "${APPDIR}/opt/stargate/scripts/stargate" "$@"
