2b6e9a529d0714e05207c326665eb1412e9e095e
Anon McHacker <anonymous@noreply.L3337.github.com>
2023-11-24 23:42:48 -0800

    AppImage: Fix Qt platform plugin failure for xcb on X11

--- src/appimage/python-appimage/stargate/entrypoint.sh
+++ src/appimage/python-appimage/stargate/entrypoint.sh
@@ -8,5 +8,15 @@ fi
 
 export QT_QPA_PLATFORM_PLUGIN_PATH="${APPDIR}/opt/python${PYVER}/lib/python${PYVER}/site-packages/PyQt5/Qt5/plugins/platforms"
 
+export QT_DEBUG_PLUGINS=1
+
+# exporting the DISPLAY fixes Qt errors regarding the xcb platform plugin
+if [ -e "$(which xrandr)" ]; then
+        export DISPLAY=":$(xrandr --current | grep -Eo 'Screen [0-9]+:' | grep -Eo '[0-9]+')"
+elif [ -e /tmp/.X11-unix ]; then
+        export DISPLAY=$(ls /tmp/.X11-unix | tr 'X' ':' | head -n 1)
+fi
+
+
 {{ python-executable }} -s \
 	-u "${APPDIR}/opt/stargate/scripts/stargate" "$@"
